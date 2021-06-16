ef9a299c5f1e52522ae5c3ef435e03708f03dcc9
alefron <ola.front@gmail.com>
2021-06-17 01:10:20 +0200

    początki
    przycisk na nav barze i nowe pliki ts i html

---
+++ PP/ClientApp/src/app/shared/about-user.model.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.AboutUser = void 0;
+var AboutUser = /** @class */ (function () {
+    function AboutUser() {
+    }
+    return AboutUser;
+}());
+exports.AboutUser = AboutUser;
+//# sourceMappingURL=about-user.model.js.map
---
+++ PP/ClientApp/src/app/shared/about-user.model.ts
@@ -0,0 +1,9 @@
+export class AboutUser {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  passwordHash: string;
+  cardNumber: string;
+  role: number;
+}
