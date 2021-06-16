35a8d4e49a3935639700fa3c4144d8c22570e2d9
Dominikkasprzyk <43817023+Dominikkasprzyk@users.noreply.github.com>
2021-06-16 11:43:08 +0200

    Added controllers for our model

---
+++ PP/Controllers/BorrowingController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PP.Models;
+
+namespace PP.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BorrowingController : ControllerBase
+    {
+        private readonly MyContext _context;
+
+        public BorrowingController(MyContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Borrowing
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Borrowing>>> GetBorrowing()
+        {
+            return await _context.Borrowing.ToListAsync();
+        }
+
+        // GET: api/Borrowing/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Borrowing>> GetBorrowing(int id)
+        {
+            var borrowing = await _context.Borrowing.FindAsync(id);
+
+            if (borrowing == null)
+            {
+                return NotFound();
+            }
+
+            return borrowing;
+        }
+
+        // PUT: api/Borrowing/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBorrowing(int id, Borrowing borrowing)
+        {
+            if (id != borrowing.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(borrowing).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BorrowingExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Borrowing
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Borrowing>> PostBorrowing(Borrowing borrowing)
+        {
+            _context.Borrowing.Add(borrowing);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetBorrowing", new { id = borrowing.Id }, borrowing);
+        }
+
+        // DELETE: api/Borrowing/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBorrowing(int id)
+        {
+            var borrowing = await _context.Borrowing.FindAsync(id);
+            if (borrowing == null)
+            {
+                return NotFound();
+            }
+
+            _context.Borrowing.Remove(borrowing);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool BorrowingExists(int id)
+        {
+            return _context.Borrowing.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ PP/Controllers/ProlongationRequestController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PP.Models;
+
+namespace PP.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProlongationRequestController : ControllerBase
+    {
+        private readonly MyContext _context;
+
+        public ProlongationRequestController(MyContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ProlongationRequest
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProlongationRequest>>> GetProlongationRequest()
+        {
+            return await _context.ProlongationRequest.ToListAsync();
+        }
+
+        // GET: api/ProlongationRequest/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProlongationRequest>> GetProlongationRequest(int id)
+        {
+            var prolongationRequest = await _context.ProlongationRequest.FindAsync(id);
+
+            if (prolongationRequest == null)
+            {
+                return NotFound();
+            }
+
+            return prolongationRequest;
+        }
+
+        // PUT: api/ProlongationRequest/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProlongationRequest(int id, ProlongationRequest prolongationRequest)
+        {
+            if (id != prolongationRequest.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(prolongationRequest).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProlongationRequestExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/ProlongationRequest
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<ProlongationRequest>> PostProlongationRequest(ProlongationRequest prolongationRequest)
+        {
+            _context.ProlongationRequest.Add(prolongationRequest);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetProlongationRequest", new { id = prolongationRequest.Id }, prolongationRequest);
+        }
+
+        // DELETE: api/ProlongationRequest/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProlongationRequest(int id)
+        {
+            var prolongationRequest = await _context.ProlongationRequest.FindAsync(id);
+            if (prolongationRequest == null)
+            {
+                return NotFound();
+            }
+
+            _context.ProlongationRequest.Remove(prolongationRequest);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ProlongationRequestExists(int id)
+        {
+            return _context.ProlongationRequest.Any(e => e.Id == id);
+        }
+    }
+}
