36cd712c2a55e6cc8040ff8a8e1b679d4aa96b85
Mateusz Marczewski <43947266+Sleyn2@users.noreply.github.com>
2021-06-21 20:45:39 +0200

    changed token time

--- PP/Controllers/ApplicationUserController.cs
+++ PP/Controllers/ApplicationUserController.cs
@@ -65,7 +65,7 @@ namespace PP.Controllers
                     {
                         new Claim("UserID",user.Id.ToString())
                     }),
-                    Expires = DateTime.UtcNow.AddDays(1),
+                    Expires = DateTime.UtcNow.AddMinutes(10),
                     SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_appSettings.JWT_Secret)),
                     SecurityAlgorithms.HmacSha256Signature)
                 };
