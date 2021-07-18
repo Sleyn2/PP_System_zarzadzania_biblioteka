e4d8cef9bc4e78e4c7f057f85c1e4c9b1e20574e
Wojtiszko <edzio47@interia.pl>
2021-07-18 18:13:23 +0200

    Podgląd książki
    - Stworzony został przycisk wraz z metodą do rezerwacji książki.

--- PP/ClientApp/src/app/book-details/book-detail-card/book-detail-card.component.ts
+++ PP/ClientApp/src/app/book-details/book-detail-card/book-detail-card.component.ts
@@ -1,10 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BookDetail } from 'src/app/shared/book-detail.model';
 import { BookDetailService } from 'src/app/shared/book-detail.service';
-import { DatePipe, Location } from '@angular/common';
-import { Borrowing } from 'src/app/shared/borrowing.model';
-import { BorrowingService } from 'src/app/shared/borrowing.service';
-
 
 @Component({
   selector: 'app-book-detail-card',
@@ -13,7 +9,7 @@ import { BorrowingService } from 'src/app/shared/borrowing.service';
 })
 export class BookDetailCardComponent implements OnInit {
 
-  constructor(private bookService: BookDetailService, private borrowingService: BorrowingService) { }
+  constructor(private bookService: BookDetailService) { }
   bookId: number = 2;
   cardData: BookDetail = new BookDetail();
   ngOnInit() {
@@ -29,10 +25,6 @@ export class BookDetailCardComponent implements OnInit {
       {
         this.cardData.count = this.cardData.count-1;
         this.bookService.updateBook(this.cardData).subscribe();
-        var borrowing = new Borrowing();
-        borrowing.BookId= this.bookId;
-        this.borrowingService.addBorrowing(borrowing).subscribe();
-
       }
       else{
       }
