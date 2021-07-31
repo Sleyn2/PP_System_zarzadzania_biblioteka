3383e6522465251b6cd68d685217b004072cd13c
Mateusz Marczewski <43947266+Sleyn2@users.noreply.github.com>
2021-07-31 19:51:15 +0200

    fix

--- PP/Controllers/BorrowingController.cs
+++ PP/Controllers/BorrowingController.cs
@@ -1,13 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PP.Models;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using PP.Models;
 
 namespace PP.Controllers
 {
