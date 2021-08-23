1d0fba682c22628914e62a2109d98abb4e5e9d48
Mateusz Marczewski <m.marczewski@wasko.pl>
2021-08-23 15:20:31 +0200

    dodanie api dal wyszukiwania autora po imieniu i nazwisku w celu walidacji

--- PP/Controllers/AuthorController.cs
+++ PP/Controllers/AuthorController.cs
@@ -7,99 +7,113 @@ using System.Threading.Tasks;
 
 namespace PP.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class AuthorController : ControllerBase
-    {
-        private readonly MyContext _context;
+	[Route("api/[controller]")]
+	[ApiController]
+	public class AuthorController : ControllerBase
+	{
+		private readonly MyContext _context;
 
-        public AuthorController(MyContext context)
-        {
-            _context = context;
-        }
+		public AuthorController(MyContext context)
+		{
+			_context = context;
+		}
 
-        // GET: api/Author
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Author>>> GetAuthor()
-        {
-            return await _context.Author.ToListAsync();
-        }
+		// GET: api/Author
+		[HttpGet]
+		public async Task<ActionResult<IEnumerable<Author>>> GetAuthor()
+		{
+			return await _context.Author.ToListAsync();
+		}
 
-        // GET: api/Author/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Author>> GetAuthor(int id)
-        {
-            var author = await _context.Author.FindAsync(id);
+		// GET: api/Author/5
+		[HttpGet("{id}")]
+		public async Task<ActionResult<Author>> GetAuthor(int id)
+		{
+			var author = await _context.Author.FindAsync(id);
 
-            if (author == null)
-            {
-                return NotFound();
-            }
+			if (author == null)
+			{
+				return NotFound();
+			}
 
-            return author;
-        }
+			return author;
+		}
+		// GET: api/Author/{name}
+		[HttpGet("find/{firstName}/{lastName}")]
+		public async Task<ActionResult<bool>> GetAuthor(string firstName, string lastName)
+		{
+			var author = await _context.Author.ToListAsync();
 
-        // PUT: api/Author/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutAuthor(int id, Author author)
-        {
-            if (id != author.Id)
-            {
-                return BadRequest();
-            }
+			if (author == null)
+			{
+				return false;
+			}
+			if (author.Where(x => x.FirstName == firstName && x.LastName == lastName).ElementAt(0) != null)
+				return true;
+			return false;
+		}
 
-            _context.Entry(author).State = EntityState.Modified;
+		// PUT: api/Author/5
+		// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+		[HttpPut("{id}")]
+		public async Task<IActionResult> PutAuthor(int id, Author author)
+		{
+			if (id != author.Id)
+			{
+				return BadRequest();
+			}
 
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!AuthorExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+			_context.Entry(author).State = EntityState.Modified;
 
-            return NoContent();
-        }
+			try
+			{
+				await _context.SaveChangesAsync();
+			}
+			catch (DbUpdateConcurrencyException)
+			{
+				if (!AuthorExists(id))
+				{
+					return NotFound();
+				}
+				else
+				{
+					throw;
+				}
+			}
 
-        // POST: api/Author
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<Author>> PostAuthor(Author author)
-        {
-            _context.Author.Add(author);
-            await _context.SaveChangesAsync();
+			return NoContent();
+		}
 
-            return CreatedAtAction("GetAuthor", new { id = author.Id }, author);
-        }
+		// POST: api/Author
+		// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+		[HttpPost]
+		public async Task<ActionResult<Author>> PostAuthor(Author author)
+		{
+			_context.Author.Add(author);
+			await _context.SaveChangesAsync();
 
-        // DELETE: api/Author/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteAuthor(int id)
-        {
-            var author = await _context.Author.FindAsync(id);
-            if (author == null)
-            {
-                return NotFound();
-            }
+			return CreatedAtAction("GetAuthor", new { id = author.Id }, author);
+		}
 
-            _context.Author.Remove(author);
-            await _context.SaveChangesAsync();
+		// DELETE: api/Author/5
+		[HttpDelete("{id}")]
+		public async Task<IActionResult> DeleteAuthor(int id)
+		{
+			var author = await _context.Author.FindAsync(id);
+			if (author == null)
+			{
+				return NotFound();
+			}
 
-            return NoContent();
-        }
+			_context.Author.Remove(author);
+			await _context.SaveChangesAsync();
 
-        private bool AuthorExists(int id)
-        {
-            return _context.Author.Any(e => e.Id == id);
-        }
-    }
+			return NoContent();
+		}
+
+		private bool AuthorExists(int id)
+		{
+			return _context.Author.Any(e => e.Id == id);
+		}
+	}
 }
