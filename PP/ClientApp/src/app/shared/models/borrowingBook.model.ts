eb74531466fcaa593f2f1f6df173e1407c13da11
Mateusz Marczewski <43947266+Sleyn2@users.noreply.github.com>
2021-08-26 17:17:28 +0200

    fix

--- PP/ClientApp/src/app/shared/models/borrowingBook.model.ts
+++ PP/ClientApp/src/app/shared/models/borrowingBook.model.ts
@@ -1,6 +1,6 @@
 export class BorrowingBook {
     id: number = 0;
-    status: number = 0;
+    status: string = "";
     bookName: string = "";
     checkoutDate: Date = null;
     checkInDate: Date = null;
