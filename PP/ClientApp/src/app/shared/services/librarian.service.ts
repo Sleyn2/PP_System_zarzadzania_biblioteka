a74946b56ae1b3e0fe372a13c22980db9abb3c7b
Dominikkasprzyk <43817023+Dominikkasprzyk@users.noreply.github.com>
2021-08-30 13:31:22 +0200

    Lista bibliotekarzy

---
+++ PP/ClientApp/src/app/shared/services/librarian.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http'
+import { User } from '../models/user.model';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LibrarianService {
+
+  constructor(private http:HttpClient) { }
+
+  formData: User = new User();
+  readonly baseUrl = 'https://localhost:44326/api/ApplicationUser'
+  list: User[];
+
+  getAllLibrarians()
+  {
+    this.http.get(this.baseUrl)
+    .toPromise()
+    .then(res => this.list = res as User[]);
+  }
+
+  getLibrarians(name:string)
+  {
+    if(name==='')
+    {
+      this.getAllLibrarians()
+    } 
+    else
+    {
+    this.http.get(this.baseUrl+ '/s/' + name)
+    .toPromise()
+    .then(res => this.list = res as User[]);
+    }
+  }
+
+  private httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+}
