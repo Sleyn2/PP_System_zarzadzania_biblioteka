3051f8a17241fede4db57d2cd16594ae0d5087ed
Mateusz Marczewski <m.marczewski@wasko.pl>
2021-09-07 08:34:25 +0200

    dodawanie nowego użytkownika przez admin panel

--- PP/ClientApp/src/app/user/role-registration/role-registration.component.ts
+++ PP/ClientApp/src/app/user/role-registration/role-registration.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { NgbActiveModal } from "@ng-bootstrap/ng-bootstrap";
+import { ToastrService } from "ngx-toastr";
 import { UserService } from "src/app/shared/services/user.service";
 
 
@@ -10,9 +11,16 @@ import { UserService } from "src/app/shared/services/user.service";
 export class RegisterModalComponent {
 
     constructor(
+        private _toastr: ToastrService,
         public activeModal: NgbActiveModal,
         private _userService: UserService
     ) { }
+
+    public selectedRole: number;
+
+    ngOnInit() {
+        this.selectedRole = 1;
+    }
 
     public body = {
         UserName: "",
@@ -22,6 +30,33 @@ export class RegisterModalComponent {
     };
 
     save() {
-        this._userService.registerWithRole(2, this.body).subscribe();
+        if (this.body) {
+            if (this.body.Password.length > 3 && this.body.UserName.length > 0) {
+                this._userService.registerWithRole(this.selectedRole, this.body).subscribe(
+                    (res: any) => {
+                        if (res.succeeded) {
+                            this._toastr.success('Utworzono nowego użytkownika!', 'Sukces.');
+                            this.activeModal.close()
+                        } else {
+                            res.errors.forEach(element => {
+                                switch (element.code) {
+                                    case 'DuplicateUserName':
+                                        this._toastr.error('Nazwa użytkownika zajęta.', 'Niepowodzenie.')
+                                        break;
+                                    default:
+                                        this._toastr.error(element.description, 'Niepowodzenie.')
+
+                                        break;
+
+                                }
+                            })
+                        }
+                    },
+                    err => {
+                        console.log(err);
+                    }
+                )
+            }
+        }
     }
 }
